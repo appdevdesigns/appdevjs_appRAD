3e598f9617339e19f18b2172d4276572368255c6
Amy Kamrath <amy@appdevdesigns.net>
2013-04-23 10:55:30 -0400

    remove unit tests from appRAD

--- web/pages/portal/tests/site_language_test_mocha.js
+++
@@ -1,8 +0,0 @@
-
-	describe('site language', function(){
-		 it('site language findAll', function(){
-		    site.Language.findAll({},function(list){
-		    	chai.assert.deepEqual(list.length,2);
-		  	});
-		 })
-	})
